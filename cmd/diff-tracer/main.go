12428b0341c49a8de7753bb1f1b4109f63a17ac3
Beto <beto@2sidesof1.com>
2024-11-12 18:14:28 -0300

    feat: raw json format

--- cmd/diff-tracer/main.go
+++ cmd/diff-tracer/main.go
@@ -1,7 +1,110 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func getPrevChar(idx int, data string) string {
+	if idx == 0 {
+		return ""
+	}
+	return fmt.Sprintf("%c", data[idx-1])
+}
+
+func getNextChar(idx int, size int, data string) string {
+	if idx == size-1 {
+		return ""
+	}
+	return fmt.Sprintf("%c", data[idx+1])
+}
 
 func main() {
-	fmt.Println("test")
+	result := ""
+	data := `{"data":[],"meta":{"numberOfUnreadNotifications":0,"total":0,"anotherArr":[1,2],"exists":false},hash:{}}`
+	currIndentation := 0
+
+	for idx, c := range data {
+		char := fmt.Sprintf("%c", c)
+
+		if char == "{" {
+			nextChar := getNextChar(idx, len(data), data)
+			if nextChar == "}" {
+				result += char
+				continue
+			}
+			result += char + "\n"
+			currIndentation += 2
+			for range currIndentation {
+				result += " "
+			}
+			continue
+		}
+
+		if char == "}" {
+			prevChar := getPrevChar(idx, data)
+			if prevChar == "{" {
+				result += char
+				continue
+			}
+			currIndentation -= 2
+			result += "\n"
+			for range currIndentation {
+				result += " "
+			}
+			result += char
+			continue
+		}
+
+		if char == "[" {
+			nextChar := getNextChar(idx, len(data), data)
+			if nextChar == "]" {
+				result += char
+				continue
+			}
+			result += char + "\n"
+			currIndentation += 2
+			for range currIndentation {
+				result += " "
+			}
+			continue
+		}
+
+		if char == "]" {
+			prevChar := getPrevChar(idx, data)
+			if prevChar == "[" {
+				result += char
+				continue
+			}
+			nextChar := getNextChar(idx, len(data), data)
+			currIndentation -= 2
+			if nextChar == "," {
+				result += "\n"
+				for range currIndentation {
+					result += " "
+				}
+				result += char
+			} else {
+				result += char + "\n"
+			}
+			continue
+		}
+
+		if char == "," {
+			result += char + "\n"
+			for range currIndentation {
+				result += " "
+			}
+			continue
+		}
+
+		// spaces
+		if char == ":" {
+			result += char + " "
+			continue
+		}
+
+		result += char
+	}
+
+	fmt.Println(result)
 }
